6ab989431d5e71e66a41b97fbf3bc8eb1c305a24
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-13 11:55:45 +0900

    feat: テキストを追加

--- src/MyVideo.tsx
+++ src/MyVideo.tsx
@@ -1,11 +1,17 @@
-import {useCurrentFrame} from "remotion";
+import { useCurrentFrame, useVideoConfig } from "remotion";
 
 export const MyVideo = () => {
   const frame = useCurrentFrame();
+  const {fps, durationInFrames, width, height} = useVideoConfig();
 
   return (
-    <div style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-      The current frame is {frame}.
-    </div>
+    <>
+      <div style={{flex: 1, justifyContent: "center", alignItems: "center", color: "white"}}>
+        The current frame is {frame}.
+      </div>
+      <div style={{flex: 1, justifyContent: "center", alignItems: "center", color: "white"}}>
+        The video is {durationInFrames / fps} seconds long.
+      </div>
+    </>
   )
 }
